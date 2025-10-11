1fcf19e02711fc7005c11c9314057b9cebe3c503
Anmol <anmolvirdi20@gmail.com>
2025-10-10 20:19:11 -0700

    update json

--- main.go
+++ main.go
@@ -4,18 +4,21 @@ import (
 	"time" 
 	"github.com/gofiber/fiber/v2"
 )
-
+/*
 type MT struct {
 	Message string `json:"message"`
 	Timestamp int64 `json:"timestamp"`
 }
+*/
 
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(MT{"My name is Anmol Virdi", time.Now().UnixMilli()})
+		return c.JSON(fiber.Map{
+			"message":"My name is Anmol Virdi",
+			"timestamp": time.Now().UnixMilli()})
 	})
 
-	app.Listen(":80")
+	app.Listen(":3000")
 }
