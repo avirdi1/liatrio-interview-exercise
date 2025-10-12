8a0d68ac312b7cc635214bffe249cf8cc78c1603
Anmol <anmolvirdi20@gmail.com>
2025-10-11 19:41:18 -0700

    update

--- main.go
+++ main.go
@@ -11,28 +11,29 @@ type MT struct {
 	Message string `json:"message"`
 	Timestamp int64 `json:"timestamp"`
 }
-*/
+
 type Resp struct {
     Message   string `json:"message"`
     Timestamp int64  `json:"timestamp"`
 }
+*/
 
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		/*return c.JSON(fiber.Map{
+		return c.JSON(fiber.Map{
 			"message":"My name is Anmol Virdi",
-			"timestamp": time.Now().UnixMilli()})
-		
+			"timestamp":time.Now().UnixMilli()})
+			/*
 		data := fiber.Map{
 			"message":"My name is Anmol Virdi",
 			"timestamp": time.Now().UnixMilli(),
 		}
 		b, _ := json.Marshal(data)
-		c.Set("Content-Type", "application/json")
+		c.Set("Content-Type", "application/json")s
 		return c.Send(b)
-		*/
+		
 		    r := Resp{
         Message:   "My name is Anmol Virdi",
         Timestamp: time.Now().UnixMilli(),
@@ -41,6 +42,7 @@ func main() {
 		c.Response().Header.SetContentType(fiber.MIMEApplicationJSON)
 		c.Response().SetBodyRaw(b) // send bytes exactly
 		return nil
+		*/
 	})
 
 	app.Listen(":80")
