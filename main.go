32de8814e36c8681eb96ce2d51f4fc5cf6f2d705
Anmol <anmolvirdi20@gmail.com>
2025-10-13 22:12:07 -0700

    add comment

--- main.go
+++ main.go
@@ -1,21 +1,10 @@
 package main
 
 import (
-	
+	// To be able to use unix time and be able to use the Fiber framework
 	"time" 
 	"github.com/gofiber/fiber/v2"
 )
-/*
-type MT struct {
-	Message string `json:"message"`
-	Timestamp int64 `json:"timestamp"`
-}
-
-type Resp struct {
-    Message   string `json:"message"`
-    Timestamp int64  `json:"timestamp"`
-}
-*/
 
 func main() {
 	app := fiber.New()
@@ -24,24 +13,6 @@ func main() {
 		return c.JSON(fiber.Map{
 			"message":"My name is Anmol Virdi",
 			"timestamp":time.Now().UnixMilli()})
-			/*
-		data := fiber.Map{
-			"message":"My name is Anmol Virdi",
-			"timestamp": time.Now().UnixMilli(),
-		}
-		b, _ := json.Marshal(data)
-		c.Set("Content-Type", "application/json")s
-		return c.Send(b)
-		
-		    r := Resp{
-        Message:   "My name is Anmol Virdi",
-        Timestamp: time.Now().UnixMilli(),
-		}
-		b, _ := json.Marshal(r) // compact JSON, no spaces or newline
-		c.Response().Header.SetContentType(fiber.MIMEApplicationJSON)
-		c.Response().SetBodyRaw(b) // send bytes exactly
-		return nil
-		*/
 	})
 
 	app.Listen(":80")
