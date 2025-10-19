24d6faacaae4b2453d317b494f494a252b122c70
Anmol <anmolvirdi20@gmail.com>
2025-10-19 01:18:54 -0700

    add in comments

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-	// To be able to use unix time and be able to use the Fiber framework
 	"time" 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	app := fiber.New()
+	app := fiber.New() 
 
+	// if someone sends a get request to the path '/', it runs the handler function which gives us access to HTTP request and response
 	app.Get("/", func(c *fiber.Ctx) error {
+		// more convenient to use fiber.Map than a struct
 		return c.JSON(fiber.Map{
 			"message":"My name is Anmol Virdi",
 			"timestamp":time.Now().UnixMilli(),
 			"quote":"To be or not to be"})
 	})
 
-	app.Listen(":80")
+	app.Listen(":80") 
 }
